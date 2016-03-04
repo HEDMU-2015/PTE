2621e2f709c011fda08f7efc07878156bd65512f
tsvetelintsonev <github.tsonev@yahoo.co.uk>
2016-03-04 11:18:42 +0100

    LogicFactory er lavet samt LogicFactoryImpl klassen. PTEController er opdateret.

---
+++ PTE/Implemenation/Logic/LogicFactory.java
@@ -0,0 +1,10 @@
+package Logic;
+
+public interface LogicFactory {
+	public Vinkel createVinkel();
+	public Vaegt createVaegt();
+	public Tyngdekraft craeteTyngdeKraft();
+	public DimensionerendeKraft craeteDimensionerendeKraft(Vaegt vaegt, Tyngdekraft tyngdeKraft);
+	public Forskydningskraft createForskydningskraft(Vinkel vinkel, DimensionerendeKraft dimensionerendeKraft);
+	public Normalkraft createNormalKraft(DimensionerendeKraft dimensionerendeKraft, Vinkel vinkel);
+}
