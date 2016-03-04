baf855b2eb3830e54370a92adf8a746fbee6edca
Zeltres <zeltr@DESKTOP-J0IIDR8.ceuherning.int>
2016-03-04 10:45:02 +0100

    DimensionerendeKraftImpl opdateret

--- PTE/Implemenation/Logic/DimensionerendeKraftImpl.java
+++ PTE/Implemenation/Logic/DimensionerendeKraftImpl.java
@@ -7,29 +7,28 @@ class DimensionerendeKraftImpl implements DimensionerendeKraft {
 	private Vaegt vaegt;
 	private Tyngdekraft tyngdekraft;
 
-	public DimensionerendeKraftImpl(Vaegt vaegt, Tyngdekraft tyngdekraft){
+	public DimensionerendeKraftImpl(Vaegt vaegt, Tyngdekraft tyngdekraft) {
 		this.vaegt = vaegt;
 		this.tyngdekraft = tyngdekraft;
 	}
-	
-	
+
 	@Override
 	public double getDimensionerendeKraft() {
 		dimensionerendeKraft = vaegt.getVaegt() * tyngdekraft.getTyngdekraft();
 		return dimensionerendeKraft;
-	} 
-	
+	}
+
 	@Override
 	public void setDimensionerendeKraft(double dimensionerendeKraft) {
 		this.dimensionerendeKraft = dimensionerendeKraft;
 	}
-	
+
 	@Override
 	public double getDimensionerendeKraftTilVaegt() {
 		vaegtFraDimensionerendeKraft = dimensionerendeKraft / tyngdekraft.getTyngdekraft();
 		return vaegtFraDimensionerendeKraft;
 	}
-	
+
 	@Override
 	public void setDimensionerendeKraftTilVargt(double vaegtFraDimensionerendeKraft) {
 		this.vaegtFraDimensionerendeKraft = vaegtFraDimensionerendeKraft;
