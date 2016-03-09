5ec4adf6c5822c6563dd9463f0002f7032b0fbe9
denierus <denieru@live.dk>
2016-03-09 11:03:52 +0100

    Test Tyngdekraft og Tyngdekraftimpl kaster exceptions

--- PTE/Implemenation/Logic/TyngdekraftImpl.java
+++ PTE/Implemenation/Logic/TyngdekraftImpl.java
@@ -1,11 +1,17 @@
 package Logic;
 
+import Exceptions.TyngdekraftException;
+import Exceptions.VaegtException;
+
 class TyngdekraftImpl extends PTEEntityImpl implements Tyngdekraft {
 
 	private double tyngdekraft = 9.816;
 	
 	@Override
 	public double getTyngdekraft() {
+		if(tyngdekraft<=0){
+			throw new TyngdekraftException();
+		}
 		return tyngdekraft;
 	}
 
