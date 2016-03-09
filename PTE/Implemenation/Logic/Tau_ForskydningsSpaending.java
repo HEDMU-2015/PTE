04761c7d095178f6edc6223111c7de06284c0d05
Juyoung Choi <Juyoung Choi@LAPTOP-QIKL91OU>
2016-03-09 19:57:36 +0100

    logic

---
+++ PTE/Implemenation/Logic/Tau_ForskydningsSpaending.java
@@ -0,0 +1,9 @@
+package Logic;
+
+interface Tau_ForskydningsSpaending extends PTEEntity{
+
+	public void setTau_ForskydningsSpaending(double tau_Forskydningsspaending);
+	public double getTau_ForskydningsSpaending();
+	public void nulstil();
+	
+}
---
+++ PTE/Implemenation/Logic/Tau_ForskydningsSpaendingImpl.java
@@ -0,0 +1,55 @@
+package Logic;
+
+
+import Exceptions.UdefineretProfilException;
+/**
+ * 
+ * @author Juyoung Choi
+ *
+ */
+
+public class Tau_ForskydningsSpaendingImpl extends PTEEntityImpl implements Tau_ForskydningsSpaending {
+	private double tau_ForskydningsSpaending = Double.NaN;
+	private Forskydningskraft forsykdningskraft;
+	private Areal areal;
+	
+	
+
+	public Tau_ForskydningsSpaendingImpl (Areal areal, Forskydningskraft forskydningskraft){		
+		this.areal = areal;
+		this.forsykdningskraft = forskydningskraft;
+		this.areal.tilfoejAfhaengigEntitet(this);
+		this.forsykdningskraft.tilfoejAfhaengigEntitet(this);
+
+	}
+
+
+	@Override
+	public void setTau_ForskydningsSpaending(double tau_ForskydningsSpaending) {
+		this.tau_ForskydningsSpaending = tau_ForskydningsSpaending;		
+	}
+
+	@Override
+	public double getTau_ForskydningsSpaending() {
+		//What is the difference between throws or try-catch? interface doesn't show it... isn't it better?
+		//We check if the areal <= 0 before, so no need to have if{} here
+		try {
+			tau_ForskydningsSpaending = forsykdningskraft.getForskydningskraft() / areal.getAreal();
+		} catch (UdefineretProfilException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tau_ForskydningsSpaending;
+	}
+
+	@Override
+	public void nulstil() {
+		setTau_ForskydningsSpaending(Double.NaN);
+	}
+
+
+	@Override
+	protected Tilstand getEgenAfhaengighed() {
+		return Tilstand.TAU_FORSKYDNINGSSPAENDING;
+	}
+}
