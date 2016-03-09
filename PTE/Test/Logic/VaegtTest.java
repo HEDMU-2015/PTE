5732e1de552d2c1c3790729add53ced335105833
denierus <denieru@live.dk>
2016-03-09 10:33:13 +0100

    Test Vaegt + exceptions

---
+++ PTE/Test/Logic/VaegtTest.java
@@ -0,0 +1,72 @@
+package Logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Exceptions.VaegtException;
+
+public class VaegtTest {
+
+	VaegtImpl v;
+
+	@Before
+	public void setUp() throws Exception {
+		v = new VaegtImpl();
+
+	}
+
+	@Test
+	public void setVaegtNulstil() {
+
+		v.setVaegt(5);
+		v.nulstil();
+		assertEquals(Double.NaN, v.getVaegt(), 0.001);
+	}
+
+	@Test
+	public void setVaegtNegativ() {
+		v.setVaegt(-5);
+
+		try {
+			if (v.getVaegt() <= 0) {
+				
+			} else {
+				fail("Vægt må ikke være negativt tal");
+			}
+		} catch (VaegtException e) {
+			// succes
+		}
+
+	}
+
+	@Test
+	public void setVaegtNul() {
+		v.setVaegt(5);
+
+		try {
+			if (v.getVaegt() <= 0) {
+				
+			} else {
+				fail("Vægt må ikke være negativt tal");	
+			}
+		} catch (VaegtException e) {
+			// succes
+			System.out.println("test");
+		}
+	}
+
+	@Test
+	public void setVaegtAfrunding() {
+		
+		v.setVaegt(3.03456);
+		assertEquals(3.035, v.getVaegt(), 0.001);
+	}
+
+	@Test
+	public void setVaegtDecimaltal() {
+		v.setVaegt(0.001);
+		assertEquals(0.001, v.getVaegt(), 0.001);
+	}
+}
