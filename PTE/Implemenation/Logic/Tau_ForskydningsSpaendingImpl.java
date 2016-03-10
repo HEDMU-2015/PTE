d420df7159648bb16f538ee19ce6c61b60a0ff06
Juyoung Choi <Juyoung Choi@LAPTOP-QIKL91OU>
2016-03-10 05:41:33 +0100

    Logic > forskydnigskraft rename

--- PTE/Implemenation/Logic/Tau_ForskydningsSpaendingImpl.java
+++ PTE/Implemenation/Logic/Tau_ForskydningsSpaendingImpl.java
@@ -10,16 +10,16 @@ import Exceptions.UdefineretProfilException;
 
 public class Tau_ForskydningsSpaendingImpl extends PTEEntityImpl implements Tau_ForskydningsSpaending {
 	private double tau_ForskydningsSpaending = Double.NaN;
-	private Forskydningskraft forsykdningskraft;
+	private Forskydningskraft forskydningskraft;
 	private Areal areal;
 	
 	
 
 	public Tau_ForskydningsSpaendingImpl (Areal areal, Forskydningskraft forskydningskraft){		
 		this.areal = areal;
-		this.forsykdningskraft = forskydningskraft;
+		this.forskydningskraft = forskydningskraft;
 		this.areal.tilfoejAfhaengigEntitet(this);
-		this.forsykdningskraft.tilfoejAfhaengigEntitet(this);
+		this.forskydningskraft.tilfoejAfhaengigEntitet(this);
 
 	}
 
@@ -34,7 +34,7 @@ public class Tau_ForskydningsSpaendingImpl extends PTEEntityImpl implements Tau_
 		//What is the difference between throws or try-catch? interface doesn't show it... isn't it better?
 		//We check if the areal <= 0 before, so no need to have if{} here
 		try {
-			tau_ForskydningsSpaending = forsykdningskraft.getForskydningskraft() / areal.getAreal();
+			tau_ForskydningsSpaending = forskydningskraft.getForskydningskraft() / areal.getAreal();
 		} catch (UdefineretProfilException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
