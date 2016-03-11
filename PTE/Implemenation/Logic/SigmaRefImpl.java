1f2a053ad1241ec360752e45b32044f8c76444ad
Zeltres <zeltr@DESKTOP-J0IIDR8.ceuherning.int>
2016-03-11 10:01:15 +0100

    Afhængigheder er tiføjet! \o/

--- PTE/Implemenation/Logic/SigmaRefImpl.java
+++ PTE/Implemenation/Logic/SigmaRefImpl.java
@@ -2,27 +2,33 @@ package Logic;
 
 public class SigmaRefImpl extends PTEEntityImpl implements SigmaRef {
 
-	private double sigmaRef;
+	private double sigmaRef = Double.NaN;
 	private SigmaB sigmaB;
 	private SigmaN sigmaN;
 	private Tau_ForskydningsSpaending tau_ForskydningsSpaendingen;
 
 	public SigmaRefImpl(SigmaB sigmaB, SigmaN sigmaN, Tau_ForskydningsSpaending tau_ForskydningsSpaendingen) {
 		this.sigmaB = sigmaB;
+		this.sigmaB.tilfoejAfhaengigEntitet(this);
 		this.sigmaN = sigmaN;
+		this.sigmaN.tilfoejAfhaengigEntitet(this);
 		this.tau_ForskydningsSpaendingen = tau_ForskydningsSpaendingen;
+		this.tau_ForskydningsSpaendingen.tilfoejAfhaengigEntitet(this);
 	}
 
 	@Override
 	public double getSigmaRef() {
-		return sigmaRef = Math.sqrt(Math.pow(sigmaB.getSigmaB() + sigmaN.getSigmaN(), 2)
+		sigmaRef = Math.sqrt(Math.pow(sigmaB.getSigmaB() + sigmaN.getSigmaN(), 2)
 				+ 3 * tau_ForskydningsSpaendingen.getTau_ForskydningsSpaending());
+
+		return sigmaRef;
 	}
 
 	@Override
 	public void nulstil() {
 		setSigmaRef(Double.NaN);
 	}
+
 	@Override
 	public void setSigmaRef(double sigmaRef) {
 		this.sigmaRef = sigmaRef;
