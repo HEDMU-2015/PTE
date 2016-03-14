249e9b0c7be6d401a32b5f2e54d3324ab70badef
etharion <warlordcp@gmail.com>
2016-03-14 11:53:25 +0100

    Scrollbar  i gui

--- PTE/Implemenation/gui/Main.java
+++ PTE/Implemenation/gui/Main.java
@@ -8,16 +8,20 @@ import Logic.PTEControllerImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Stage stage;
 	private BorderPane mainWindow;
+	private ScrollPane scrollPane;
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -38,6 +42,7 @@ public class Main extends Application {
 			mainWindow = (BorderPane) loader.load();
 			Scene scene = new Scene(mainWindow);
 			stage.setScene(scene);
+			stage.setResizable(false);
 			stage.show();
 		} catch (IOException exc) {
 		}
@@ -52,6 +57,14 @@ public class Main extends Application {
 			loaderUC1_UC2.setLocation(Main.class.getResource("PaneUC1_UC2.fxml"));
 			AnchorPane paneUC1_UC2 = (AnchorPane) loaderUC1_UC2.load();
 			TitledPane tpUC1_UC2 = new TitledPane("Normalkraft / Forskydningskraft", paneUC1_UC2);
+			tpUC1_UC2.setExpanded(false);
+			tpUC1_UC2.setOnMouseReleased(e -> {
+				if (tpUC1_UC2.getHeight() < 225 ) {
+				tpUC1_UC2.setMinHeight(225);
+				} else {
+					tpUC1_UC2.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC1_UC2 = loaderUC1_UC2.<PTEPane> getController();
 			controllerPaneUC1_UC2.setPTEController(pteController);
 
@@ -61,6 +74,13 @@ public class Main extends Application {
 			AnchorPane paneUC3 = (AnchorPane) loaderUC3.load();
 			TitledPane tpUC3 = new TitledPane("Bøjningsmoment", paneUC3);
 			tpUC3.setExpanded(false);
+			tpUC3.setOnMouseReleased(e -> {
+				if (tpUC3.getHeight() < 199 ) {
+				tpUC3.setMinHeight(199);
+				} else {
+					tpUC3.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC3 = loaderUC3.<PTEPane> getController();
 			controllerPaneUC3.setPTEController(pteController);
 
@@ -70,6 +90,13 @@ public class Main extends Application {
 			AnchorPane paneUC4 = (AnchorPane) loaderUC4.load();
 			TitledPane tpUC4 = new TitledPane("Forskydningsspændingen", paneUC4);
 			tpUC4.setExpanded(false);
+			tpUC4.setOnMouseReleased(e -> {
+				if (tpUC4.getMinHeight() < 199 ) {
+				tpUC4.setMinHeight(199);
+				} else {
+					tpUC4.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC4 = loaderUC4.<PTEPane> getController();
 			controllerPaneUC4.setPTEController(pteController);
 
@@ -79,6 +106,13 @@ public class Main extends Application {
 			AnchorPane paneUC5 = (AnchorPane) loaderUC5.load();
 			TitledPane tpUC5 = new TitledPane("SigmaN", paneUC5);
 			tpUC5.setExpanded(false);
+			tpUC5.setOnMouseReleased(e -> {
+				if (tpUC5.getMinHeight() < 199 ) {
+				tpUC5.setMinHeight(199);
+				} else {
+					tpUC5.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC5 = loaderUC5.<PTEPane> getController();
 			controllerPaneUC5.setPTEController(pteController);
 						
@@ -88,6 +122,13 @@ public class Main extends Application {
 			AnchorPane paneUC7 = (AnchorPane) loaderUC7.load();
 			TitledPane tpUC7 = new TitledPane("SigmaB", paneUC7);
 			tpUC7.setExpanded(false);
+			tpUC7.setOnMouseReleased(e -> {
+				if (tpUC7.getMinHeight() < 199 ) {
+				tpUC7.setMinHeight(199);
+				} else {
+					tpUC7.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC7 = loaderUC7.<PTEPane> getController();
 			controllerPaneUC7.setPTEController(pteController);
 			
@@ -97,12 +138,23 @@ public class Main extends Application {
 			AnchorPane paneUC8 = (AnchorPane) loaderUC8.load();
 			TitledPane tpUC8 = new TitledPane("SigmaRef", paneUC8);
 			tpUC8.setExpanded(false);
+			tpUC8.setOnMouseReleased(e -> {
+				if (tpUC8.getMinHeight() < 199 ) {
+				tpUC8.setMinHeight(199);
+				} else {
+					tpUC8.setMinHeight(25);
+				}
+			});
 			PTEPane controllerPaneUC8 = loaderUC8.<PTEPane> getController();
 			controllerPaneUC8.setPTEController(pteController);
 						
 			
 			vboxICenter.getChildren().addAll(tpUC1_UC2, tpUC3, tpUC4, tpUC5, tpUC7, tpUC8);
-			mainWindow.setCenter(vboxICenter);
+			scrollPane = new ScrollPane(vboxICenter);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+			scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+			mainWindow.setCenter(scrollPane);
 		} catch (IOException exc) {
 		}
 	}
